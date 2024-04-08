0ba5a8c03eaae8c32519c07a4692db479814555c
Andres Ordonez <theandtor@gmail.com>
2024-04-07 22:08:54 -0500

    added parser for body parser

--- app/server.js
+++ app/server.js
@@ -1,22 +1,26 @@
 var express = require('express') //llamamos a Express
-var app = express()               
+var app = express()
+var bodyParser = require('body-parser')        
 
 var port = process.env.PORT || 8090  // establecemos nuestro puerto
 
-app.get('/', function(req, res) {
-  res.json({ mensaje: '¡Hola Mundo!' })   
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json()) 
+
+app.get('/', function (req, res) {
+  res.status(200).json({ message: 'Estás conectado a nuestra API' })
 })
 
-app.get('/cervezas', function(req, res) {
-  res.json({ mensaje: '¡A beber cerveza!' })  
+app.get('/cervezas', function (req, res) {
+  res.json({ mensaje: '¡A beber cerveza!' })
 })
 
-app.post('/', function(req, res) {
-  res.json({ mensaje: 'Método post' })   
+app.post('/', function (req, res) {
+  res.json({ message: 'Test post ' })
 })
 
-app.delete('/', function(req, res) {
-  res.json({ mensaje: 'Método delete' })  
+app.delete('/', function (req, res) {
+  res.json({ mensaje: 'Método delete' })
 })
 
 // iniciamos nuestro servidor
